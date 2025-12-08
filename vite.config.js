6f8b4749b7c8642fdca9a9bc3c0c793aff3e0b01
SAFAL-TIWARI <safaltiwari602@gmail.com>
2025-12-09 02:49:44 +0530

    Files moved and vercel configurations

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ export default defineConfig({
     root: '.',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                about: 'about.html',
+                menu: 'menu.html',
+                contact: 'contact.html',
+                gallery: 'gallery.html',
+            },
+        },
     },
 });
